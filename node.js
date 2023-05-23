cd469fc1152c3d9286369f227248fa5386abb906
antoniuv <115102470+antoniuv@users.noreply.github.com>
2023-05-24 00:59:10 +0300

    Add files via upload

---
+++ node.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ 
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setare motor vizualizare
+app.set('view engine', 'ejs');
+
+// Pagina principală
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Cerere AJAX
+app.get('/data', (req, res) => {
+  fs.readFile('data.json', 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).send('Server error');
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// Pagina pentru erori 404
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
+app.listen(3000, () => {
+  console.log('Serverul rulează pe portul 3000');
+});
+
